503d2aabe92e08f5f3e0979aa69055c96dba10ff
Linchenn <linchan@google.com>
2022-10-03 13:59:46 -0700

    Update fg.c

--- GPGPU/fg.c
+++ GPGPU/fg.c
@@ -1,97 +1,84 @@
 #version 300 es
-
 precision highp float;
 precision highp int;
 precision highp sampler2D;
 in vec2 resultUV;
 out vec4 outputColor;
 const vec2 halfCR = vec2(0.5, 0.5);
-uniform sampler2D x;
 
 int imod(int x, int y) {
     return x - y * (x / y);
 }
-
-int idiv(int a, int b, float sign) {
-    int res = a / b;
-    int mod = imod(a, b);
-    if (sign < 0. && mod != 0) {
-    res -= 1;
-    }
-    return res;
-}
-
-vec2 uvFromFlat(int texNumR, int texNumC, int index) {
-    int texR = index / texNumC;
-    int texC = index - texR * texNumC;
-    return (vec2(texC, texR) + halfCR) / vec2(texNumC, texNumR);
-}
-vec2 packedUVfrom1D(int texNumR, int texNumC, int index) {
-    int texelIndex = index / 2;
-    int texR = texelIndex / texNumC;
-    int texC = texelIndex - texR * texNumC;
-    return (vec2(texC, texR) + halfCR) / vec2(texNumC, texNumR);
-}
-
+    
 vec2 packedUVfrom2D(int texelsInLogicalRow, int texNumR,
-    int texNumC, int row, int col) {
-    int texelIndex = (row / 2) * texelsInLogicalRow + (col / 2);
-    int texR = texelIndex / texNumC;
-    int texC = texelIndex - texR * texNumC;
-    return (vec2(texC, texR) + halfCR) / vec2(texNumC, texNumR);
-}
-
-vec2 packedUVfrom3D(int texNumR, int texNumC,
-int texelsInBatch, int texelsInLogicalRow, int b,
-int row, int col) {
-int index = b * texelsInBatch + (row / 2) * texelsInLogicalRow + (col / 2);
-int texR = index / texNumC;
-int texC = index - texR * texNumC;
-return (vec2(texC, texR) + halfCR) / vec2(texNumC, texNumR);
+  int texNumC, int row, int col) {
+  int texelIndex = (row / 2) * texelsInLogicalRow + (col / 2);
+  int texR = texelIndex / texNumC;
+  int texC = texelIndex - texR * texNumC;
+  return (vec2(texC, texR) + halfCR) / vec2(texNumC, texNumR);
 }
 
 float sampleTexture(sampler2D textureSampler, vec2 uv) {
     return texture(textureSampler, uv).r;
 }
 
-void setOutput(float val) {
-    outputColor = vec4(val, 0, 0, 0);
+
+void setOutput(vec4 val) {
+    outputColor = val;
+}
+  
+uniform sampler2D matrixA;
+uniform int offsetmatrixA;
+uniform sampler2D matrixB;
+uniform int offsetmatrixB;
+
+    ivec3 getOutputCoords() {
+      ivec2 resTexRC = ivec2(resultUV.yx *
+                             vec2(633, 632));
+      int index = resTexRC.x * 632 + resTexRC.y;
+
+      int b = index / 400000;
+      index -= b * 400000;
+
+      int r = 2 * (index / 20);
+      int c = imod(index, 20) * 2;
+
+      return ivec3(b, r, c);
+    }
+  
+
+        
+vec4 getMatrixA(int row, int col) {
+    vec2 uv = packedUVfrom2D(16, 566, 566, row, col);
+    return texture(matrixA, uv);
 }
 
-
-ivec4 getOutputCoords() {
-    ivec2 resTexRC = ivec2(resultUV.yx *
-    vec2(144, 256));
-    int index = resTexRC.x * 256 + resTexRC.y;
-    int r = index / 36864; index -= r * 36864;int c = index / 3072; index -= c * 3072;int d = index / 256; int d2 = index - d * 256;
-    return ivec4(r, c, d, d2);
+vec4 getMatrixB(int row, int col) {
+    vec2 uv = (vec2(col, row) + halfCR) / vec2(40.0, 32.0);
+    return texture(matrixB, uv);
 }
     
-float getX(int row, int col, int depth) {
-    float texR = dot(vec2(row, col), vec2(12, 1));
-    float texC = float(depth);
-    vec2 uv = (vec2(texC, texR) + halfCR) / vec2(256.0, 144.0);
-    return sampleTexture(x, uv);
-}
+// Don't use uniform for sharedDimensionPacked for performance.
+const float sharedDimension = 16.0;
 
-float getX(int row, int col, int depth, int depth2) {
-    return getX(col, depth, depth2);
+vec4 dot2x2ARowBCol(ivec3 rc) {
+    vec4 result = vec4(0);
+    for (int i = 0; i < 16; i++) {
+        int batchA = rc.x;
+        int batchB = rc.x;
+        vec4 a = getMatrixA(rc.y, i * 2);
+        vec4 b = getMatrixB(i * 2, rc.z);
+
+        // These swizzled products need to be separately added.
+        // See: https://github.com/tensorflow/tfjs/issues/1735
+        result += (a.xxzz * b.xyxy);
+        result += (a.yyww * b.zwzw);
+    }
+    return result;
 }
 
 void main() {
-    ivec4 coords = getOutputCoords();
-    int batch = coords[0];
-    int xRCorner = coords[1];
-    int xCCorner = coords[2];
-    float result = 0.0;
-
-    int flatIndexStart = (xRCorner * 12 + xCCorner) * 256;
-    for (int ch = 0; ch < 256; ch += 1) {
-        int index = flatIndexStart + ch;
-        int texR = index / 256;
-        int texC = index - texR * 256;
-        vec2 uv = (vec2(texC, texR) + halfCR) / vec2(256, 144);
-        result += uv.x + uv.y;
-    }
+    ivec3 rc = getOutputCoords();
+    vec4 result = dot2x2ARowBCol(rc);
     setOutput(result);
 }
